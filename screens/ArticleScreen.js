78f41f2d3350f64098a63a12d03c94ee490246d6
MakotoTaguchi <taguchimakoto@icloud.com>
2022-05-25 01:57:19 +0900

    main

--- screens/ArticleScreen.js
+++ screens/ArticleScreen.js
@@ -1,6 +1,9 @@
 import React from "react";
-import { SafeAreaView, StyleSheet, Text } from "react-native";
+import { SafeAreaView, StyleSheet, Text, TouchableOpacity } from "react-native";
 import { WebView } from "react-native-webview";
+import { useDispatch, useSelector } from "react-redux";
+import ClipButton from "../components/ClipButton";
+import { addClip, deleteClip } from "../store/actions/user";
 
 const styles = StyleSheet.create({
   container: {
@@ -11,8 +14,27 @@ const styles = StyleSheet.create({
 
 ArticleScreen = ({ route }) => {
   const { article } = route.params;
+
+  const dispatch = useDispatch();
+
+  const user = useSelector((state) => state.user);
+  const { clips } = user;
+
+  const isClipped = () => {
+    return clips.some((clip) => clip.url === article.url);
+  };
+
+  const toggleClip = () => {
+    if (isClipped()) {
+      dispatch(deleteClip({ clip: article }));
+    } else {
+      dispatch(addClip({ clip: article }));
+    }
+  };
+
   return (
     <SafeAreaView style={styles.container}>
+      <ClipButton onPress={toggleClip} enabled={isClipped()} />
       <WebView source={{ url: article.url }} />
     </SafeAreaView>
   );
