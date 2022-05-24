d03edb91dc324a8333d8d88f1238d066d7f9d8ad
MakotoTaguchi <taguchimakoto@icloud.com>
2022-05-25 01:56:54 +0900

    main

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 import AppNavigator from "./navigation/AppNavigator";
-import store from "./store";
+import store, { persistor } from "./store";
 
 export default function App() {
   return (
     <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor} />
       <AppNavigator />
     </Provider>
   );
